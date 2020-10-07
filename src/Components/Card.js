af753d7c14a66a8f33b282717a9a71fc299d4032
Heba <36724067+heba@users.noreply.github.com>
2020-10-07 17:09:14 +0300

    on click the cards flip

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import ReactCardFlip from "react-card-flip"; /* 1 */
+
+import CardBack from '../images/CardBack.jpg'
+
 
 const Card = ({card}) => {
+        const [flipped, setFlipped] = useState(false)
+        // let cardcover = flipped ?  CardBack : card.front;
+
         return(
             <div className="col-3 my-1">
-                <img className="mx-auto" 
+              <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+                <a href="#" > 
+                 <img className="mx-auto" 
+                    src={CardBack}
+                    width="100%"
+                    hieght="100%"
+                    onClick={()=>setFlipped(!flipped)}/>
+                </a>
+                <a href="#" > 
+                 <img className="mx-auto" 
                     src={card.front}
                     width="100%"
-                    hieght="100%"/>
+                    hieght="100%"
+                    onClick={()=>setFlipped(!flipped)}/>
+                </a>
+              </ReactCardFlip>
             </div>
         );
     };
