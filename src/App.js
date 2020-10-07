dc4319adaf83f12a5a0548cac378d6f4e2b143af
Heba <36724067+heba@users.noreply.github.com>
2020-10-07 13:57:09 +0300

    Show a single card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./App.css";
 
+import cards from './data.js'
+import CardBack from './images/CardBack.jpg'
+
 function App() {
-  return <div className="App border my-5">Place your code here</div>;
+  
+  return (
+    <div className="App border my-5">
+      <div className="container">
+
+      <div className="row">
+        <div className="col-3 my-1">
+          <img className="mx-auto" 
+               src={cards[4].front}
+               width="100%"
+               hieght="100%"
+          />
+          <img className="mx-auto" 
+               width="100%"
+               hieght="100%"
+               src={CardBack}
+          />
+        </div>
+      </div>
+     </div>
+    </div>
+  )
 }
 
 export default App;
